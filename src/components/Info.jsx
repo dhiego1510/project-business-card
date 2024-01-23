a9a8f0e3d9a9da8810d54391a24debe7706e859d
Diego <dhiego1510@gmail.com>
2024-01-22 19:23:56 -0500

    spelling correction

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -2,7 +2,7 @@ export default function Info() {
   return (
     <section className="abaout-interest" >
 
-      <h3 className="h3">Abaout</h3>
+      <h3 className="h3">About</h3>
       <p>
       I am a software developer, always focused on innovation, looking for exciting challenges to expand
        my skills and contribute to the world of software development.</p>
